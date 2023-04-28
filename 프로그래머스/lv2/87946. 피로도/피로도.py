936196475bee38f1d8bcc9fa2240918929e4c44f
bellday <92672351+bellday@users.noreply.github.com>
2023-04-29 00:54:02 +0900

    [level 2] Title: 피로도, Time: 0.08 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/87946. 피로도/피로도.py
+++ 프로그래머스/lv2/87946. 피로도/피로도.py
@@ -1,19 +1,47 @@
-#소모필요도 보다 크다면 카운팅 , k- 소모피로도를 k에 대입
-#8! 가지수 구하기
-from itertools import permutations
+
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+answer = 0
+N = 0
+visited = []
+
+
+def dfs(k, cnt, dungeons):
+    global answer
+    if cnt > answer:
+        answer = cnt
+
+    for j in range(N):
+        if k >= dungeons[j][0] and not visited[j]:
+            visited[j] = 1
+            dfs(k - dungeons[j][1], cnt + 1, dungeons)
+            visited[j] = 0
+
 
 def solution(k, dungeons):
-    dun_num = len(dungeons)
-    answer = 0
-    
-    for permut in permutations(dungeons, dun_num):
-        hp=k
-        count=0
-        for p in permut:
-            if hp>=p[0]:
-                hp=hp-p[1]
-                count+=1
-        
-        if answer<count:
-            answer=count
+    global N, visited
+    N = len(dungeons)
+    visited = [0] * N
+    dfs(k, 0, dungeons)
     return answer
