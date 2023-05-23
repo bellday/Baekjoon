ce650b08c05be87f60323aa77463a0ad33ff6c2a
bellday <92672351+bellday@users.noreply.github.com>
2023-05-24 00:35:54 +0900

    [level 2] Title: 택배상자, Time: 371.92 ms, Memory: 53.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/131704. 택배상자/택배상자.py
@@ -0,0 +1,16 @@
+def solution(order):
+    answer = 0
+    stack=[]
+    odr=0
+    i=1
+    while i != len(order)+1:
+        stack.append(i)
+        while stack and stack[-1]==order[odr]:
+            odr+=1
+            stack.pop()
+        i+=1
+    
+
+    
+        
+    return odr
