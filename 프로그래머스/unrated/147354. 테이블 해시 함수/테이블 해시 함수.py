1fa04da6d5b5f2b82e7b9b149b6093bf5a5d9610
bellday <92672351+bellday@users.noreply.github.com>
2023-05-26 19:02:22 +0900

    [unrated] Title: 테이블 해시 함수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/147354. 테이블 해시 함수/테이블 해시 함수.py
@@ -0,0 +1,22 @@
+# key ,lambda 로 정렬 구현
+# row_begin 부터 row_end 까지 for 문을 돌며 순회
+# mod 값으로 최신화
+# 합한값을 ^= 통해 xor  계산
+def solution(data, col, row_begin, row_end):
+    answer = 0
+    s=0
+    data = sorted(data , key = lambda x : (x[col-1],-x[0]))
+    
+    
+    for i in range(row_begin-1,row_end):
+        s=0
+        for j in range(len(data[i])):
+            temp = data[i][j]
+            data[i][j] = temp%(i+1)
+            
+        s+=sum(data[i])  
+        answer ^= s
+        print(answer)
+    
+    
+    return answer
