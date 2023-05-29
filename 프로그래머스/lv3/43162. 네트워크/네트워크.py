3b12a04aa1dc76a7601efd9f4f180c9f5ef47ed5
bellday <92672351+bellday@users.noreply.github.com>
2023-05-30 00:27:58 +0900

    [level 3] Title: 네트워크, Time: 0.61 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.py
@@ -0,0 +1,25 @@
+def dfs(node, graph, visit):
+    visit[node] = True
+    for i in range(len(graph)):
+        if graph[node][i] == 1 and not visit[i]:
+            dfs(i, graph, visit)
+            print(node,i)
+
+def solution(n, computers):
+    a=1
+    graph = [[0] * n for _ in range(n)]
+    visit = [False] * n
+    cnt = 0
+
+    for i in range(n):
+        for j in range(n):
+            if computers[i][j] == 1:
+                graph[i][j] = 1
+
+    for i in range(n):
+        if not visit[i]:
+            dfs(i, graph, visit)
+            cnt += 1
+    
+
+    return cnt
