df662f82bc3c2556564c9d033c51e423182ffdac
bellday <92672351+bellday@users.noreply.github.com>
2023-05-31 01:31:56 +0900

    [level 2] Title: 배달, Time: 0.07 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12978. 배달/배달.py
@@ -0,0 +1,32 @@
+import heapq
+#다익스트라
+def solution(N, road, K):
+    answer = 0
+    #거리배열설정
+    village = [1000000]*(N+1)
+    #사용 편하기위함 N+1 사용
+    graph = [[] for _ in range(N+1)]
+    
+    #그래프 설정
+    for r in road:
+        vill1, vill2, time = r
+        #비용 노드번호 추가
+        graph[vill1].append((time,vill2))
+        graph[vill2].append((time,vill1))
+    q=[]
+    village[1] = 0
+    heapq.heappush(q,(0,1))
+    while q:
+        dist , now =heapq.heappop(q)
+        if village[now]<dist:
+            continue
+        for i in graph[now]:
+            cost = dist + i[0]
+            if cost < village[i[1]]:
+                village[i[1]] =cost
+                heapq.heappush(q,(cost,i[1]))
+    #print(village)
+    for v in village:
+        if K>=v:
+            answer+=1
+    return answer
