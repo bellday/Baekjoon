ee07e3dfa9c13dcdab29cf347a3bafa5ef4fc48e
bellday <92672351+bellday@users.noreply.github.com>
2023-06-14 00:32:01 +0900

    [unrated] Title: 공배수, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181936. 공배수/공배수.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int number, int n, int m) {
+    int answer = 0;
+    if (number % n ==0 && number %m ==0){
+        answer =1;
+    }
+    else{
+        answer - 0;
+    }
+    return answer;
+}
