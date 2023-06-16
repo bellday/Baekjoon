04ee73838e83133c5519f64f0ad04390c34caa93
bellday <92672351+bellday@users.noreply.github.com>
2023-06-17 00:16:44 +0900

    [level 3] Title: 순위, Time: 145.91 ms, Memory: 10.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/49191. 순위/순위.py
@@ -0,0 +1,35 @@
+'''
+플로이드 마샬 알고리즘 사용
+승자 -> 1로 표시, 패자 -> -1 로 표시
+a-> b , b->c 이면 a->c이므로 , a->b, b->c 가 1이면 a->c 는 1로(승리) 표시 , c->a 는 -1로 표시
+최종적으로 본인은 승패를 할 수 없으므로 해당 배열의 0의 갯수가 0이면 순위를 매길 수 있다고 판단하여 출력
+
+'''
+
+def solution(n, results):
+    answer = 0
+    graph = [[0] * (n+1) for _ in range(n+1)]
+    for rst in results:
+        graph[rst[0]][rst[1]] = 1
+        graph[rst[1]][rst[0]] = -1
+    
+    answer = floyd(graph)
+    return answer
+
+def floyd(graph):
+    cnt =0
+    # 거쳐가는 노드(선수)
+    for i in range(len(graph)):
+        #시작 노드 (승자)
+        for j in range(len(graph)):
+            #종료 노드(패자)
+            for k in range(len(graph)):
+                
+                if graph[j][i] ==1 and  graph [i][k]==1 :
+                    graph[j][k] = 1
+                    graph[k][j]=-1
+    
+    for g in graph:
+        if g[1:].count(0) ==1:
+            cnt+=1
+    return cnt
