0e21277c16e767678093f9926c3220e238b39de2
jong il <92672351+bellday@users.noreply.github.com>
2023-06-30 13:46:31 +0900

    [level 3] Title: 가장 먼 노드, Time: 55.24 ms, Memory: 21.2 MB -BaekjoonHub

--- 프로그래머스/lv3/49189. 가장 먼 노드/가장 먼 노드.py
+++ 프로그래머스/lv3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -1,38 +1,33 @@
-'''
-다익스트라로 풀기
-힙 필요
-'''
 import heapq
+
 def solution(n, edge):
     answer = 0
-    Max_distance = 0 # 최대 거리 갯수
-    Range = [1000000000] *(n+1) #각 노드의 거리
-    NodeGraph = [[] for _ in range(n+1) ]
-    
-    #그래프 설정
-    for e in edge:
-        x= e[0] ; y = e[1]
-        NodeGraph[y].append(x)
-        NodeGraph[x].append(y)
-    
-    heap = [] #거리 , 노드번호
+    maximum_depth = 0
+    visit = [False] *(n+1)
+    graph = [[] for _ in range(n+1)]
+    dist_arr = [100000000] * (n+1)
+    dist_arr[1] =0
+    for e0,e1 in edge:
+        graph[e0].append(e1)
+        graph[e1].append(e0)
+    heap = []
+    #거리 , 노드
     heapq.heappush(heap,[0,1])
-    Range[1] =0
+    
     while heap:
         
-        distance, NodeNum = heapq.heappop(heap)
-        if Max_distance < distance:
-            Max_distance = distance
-        
-        
-        for key, val in enumerate(NodeGraph[NodeNum]):
+        dist, node = heapq.heappop(heap)
+        visit[node] = True
+        if maximum_depth < dist:
+            maximum_depth = dist
+        if dist_arr[node] > dist+1:
+            dist_arr[node] = dist+1
+        for i in graph[node]:
             
-            if distance +1 < Range[val]:
-                heapq.heappush(heap,[distance+1,val])
-                
-                Range[val] = distance +1
+            if visit[i] == False:
+                visit[i] = True
+                heapq.heappush(heap,[dist+1,i])
+    maximum_depth = max(dist_arr[1:])
     
-    for r in Range:
-        if r == Max_distance:
-            answer +=1
-    return answer
+    
+    return dist_arr.count(maximum_depth)
