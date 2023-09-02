f8208d9b3b4bf2ad620d4735bef9cc4b37f2ce1f
jong il <92672351+bellday@users.noreply.github.com>
2023-09-03 01:47:20 +0900

    [Silver II] Title: 최대 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+priority_queue<int,vector<int>> PQ;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int Size;
+	cin >> Size;
+
+	for (int i = 0; i < Size; i++) {
+		int InputNumber;
+		cin >> InputNumber;
+
+		if (InputNumber == 0) {
+			if (PQ.empty()) {
+				cout << 0 << "\n";
+			}
+			else {
+				cout << PQ.top() << "\n";
+				PQ.pop();
+			}
+		}
+		else {
+			
+			PQ.push(InputNumber);
+		}
+	}
+}
